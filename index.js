2d79bec50777235ad082b4833967e78013b7974c
Dominic Tarr <dominic.tarr@gmail.com>
2016-02-01 16:08:33 +1300

    oops

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-module.exports =  function (read, highWaterMark) {
-  var buffer = [], waiting = [], ended, ending, reading = false
-  highWaterMark = highWaterMark || 10
+module.exports =  function (highWaterMark) {
+  return function (read) {
+    var buffer = [], waiting = [], ended, ending, reading = false
+    highWaterMark = highWaterMark || 10
 
-  function readAhead () {
-    while(waiting.length && (buffer.length || ended))
-      waiting.shift()(ended, ended ? null : buffer.shift())
+    function readAhead () {
+      while(waiting.length && (buffer.length || ended))
+        waiting.shift()(ended, ended ? null : buffer.shift())
 
-    if (!buffer.length && ending) ended = ending;
-  }
+      if (!buffer.length && ending) ended = ending;
+    }
 
-  function next () {
-    if(ended || ending || reading || buffer.length >= highWaterMark)
-      return
-    reading = true
-    return read(ended || ending, function (end, data) {
-      reading = false
-      ending = ending || end
-      if(data != null) buffer.push(data)
+    function next () {
+      if(ended || ending || reading || buffer.length >= highWaterMark)
+        return
+      reading = true
+      return read(ended || ending, function (end, data) {
+        reading = false
+        ending = ending || end
+        if(data != null) buffer.push(data)
+
+        next(); readAhead()
+      })
+    }
+
+    ;(setImmediate || process.nextTick)(next)
+
+    return function (end, cb) {
+      ended = ended || end
+      waiting.push(cb)
 
       next(); readAhead()
-    })
-  }
-
-  ;(setImmediate || process.nextTick)(next)
-
-  return function (end, cb) {
-    ended = ended || end
-    waiting.push(cb)
-
-    next(); readAhead()
+    }
   }
 }
 
